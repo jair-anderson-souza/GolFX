a6fdf2f648e458ce6aa92d7c8097b86b6a4f4365
Anderson Souza <jair_anderson_bs@hotmail.com>
2015-12-10 13:50:08 -0300

    Visualização de produto

--- src/io/github/jass2125/core/ItemApp.java
+++ src/io/github/jass2125/core/ItemApp.java
@@ -23,16 +23,12 @@ public class ItemApp extends Application {
 	private static Produto produto;
 	private static int index;
 	private static String[] images = {
-		"http://www.sportcenterlopes.com.br/images/" +
-				"250_topper_campo_2009replic.jpg",
-				"http://1.bp.blogspot.com/_H8uGs8K8kaY/TLZTXR8nIgI/" +
-				"AAAAAAAAF_0/BvpxdqGF4PE/s1600/luva_umbro.png",
-				"http://bimg2.mlstatic.com/camisa-nike-active-importada-manga-" +
-				"longa-esportiva-vermelha_MLB-F-199843960_1391.jpg",
-				"http://www.showtenis.com.br/images/_product/979/979112/" +
-				"chuteira-nike-mercurial-glide-3-fg-campo--199fd9.jpg",
-				"http://www.katy.com.br/imagens/produtos/original/" +
-				"caneleira-topper-trainning-difusion-13340619502673137.jpg" };
+		"https://www.google.com.br/imgres?imgurl=http://www.netshoes.com.br/Produtos/32/138-9048-032/138-9048-032_detalhe1.jpg&imgrefurl=http://www.netshoes.com.br/produto/bola-topper-extreme-3-society-138-9048&h=410&w=410&tbnid=7AFE8wRVRTzomM:&docid=a3faTtWHxcSgeM&ei=LKxpVvu1E4WjwgT1npCIDg&tbm=isch&client=ubuntu&ved=0ahUKEwj7-5z73tHJAhWFkZAKHXUPBOEQMwgyKAAwAA" +
+		"https://www.google.com.br/imgres?imgurl=http://1.bp.blogspot.com/_H8uGs8K8kaY/TLZTXR8nIgI/AAAAAAAAF_0/BvpxdqGF4PE/s1600/luva_umbro.png&imgrefurl=http://guarda-metas.com/luvas/luvas-luvas/nova-luva-da-umbro-na-tka/&h=600&w=900&tbnid=jCH9TVpfb91F7M:&docid=4bBJe8WR-XuVIM&ei=SKxpVunKK8SpwgSG_brACg&tbm=isch&client=ubuntu&ved=0ahUKEwjpjuKI39HJAhXElJAKHYa-DqgQMwgyKAAwAA" +  
+		"https://www.google.com.br/imgres?imgurl=http://www.jcconfeccoes.com.br/img/produtos/5/079.jpg&imgrefurl=http://www.jcconfeccoes.com.br/index.php/prods/139/Uniformes-Esportivos-Camisas/&h=750&w=750&tbnid=hXCxMPNZHHPaaM:&docid=GQvMjXR5cszSMM&ei=bqxpVpSWJsH6wQTc86vAAw&tbm=isch&client=ubuntu" + 
+		"https://www.google.com.br/imgres?imgurl=http://images.centauro.com.br/900x900/829401Q4/chuteira-de-campo-nike-mercurial-victory-v-fg-img.jpg&imgrefurl=http://www.centauro.com.br/chuteira-de-futebol-de-campo-nike-mercurial-victory-v-fg-829401.html&h=1200&w=1200&tbnid=eKQKcWlpiuiQVM:&docid=Wpw2oS262ZWZEM&ei=hqxpVr8oibXABPPxnNAC&tbm=isch&client=ubuntu&ved=0ahUKEwi_g_-l39HJAhWJGpAKHfM4ByoQMwgyKAAwAA" + 
+		"https://www.google.com.br/imgres?imgurl=http://img.submarino.com.br/produtos/01/00/item/115494/0/115494055_2GG.jpg&imgrefurl=http://www.submarino.com.br/produto/115494055/caneleira-topper-training-2-difusion&h=500&w=500&tbnid=r02Rx_nO90ExBM:&docid=KWo0uWbR4VebVM&ei=mqxpVtLWIMWhwgSvqqDIBQ&tbm=isch&client=ubuntu"
+	};
 	
 	private void initComponents(){
 		pane = new AnchorPane();
